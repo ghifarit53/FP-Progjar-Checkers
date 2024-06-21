855806d9de01da2fe74a04b422f1c0994db47716
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2024-06-21 18:28:56 +0700

    Selection works - game does not

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+# constants.py
+
 class Constants:
     WIDTH = 600
     HEIGHT = 600
@@ -12,3 +14,6 @@ class Colors:
     DARK_SQUARE = (101, 67, 33)
     LIGHT_SQUARE = (245, 245, 245)  # Lighter color for the light squares
     HIGHLIGHT = (255, 255, 0)  # Highlight color for pieces
+    MENU_BG = (30, 30, 30)  # Menu background color
+    BUTTON_COLOR = (0, 200, 200)  # Button color
+    TEXT_COLOR = (255, 255, 255)  # Text color
